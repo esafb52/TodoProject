9cfa15ddc102d205d45405016080ce4fe958c1cf
esafb52 <esafb52@gmail.com>
2019-04-14 22:47:23 +0430

    basic config ok

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class MyTodo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    work = models.CharField(max_length=350)
+    state_complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.work
